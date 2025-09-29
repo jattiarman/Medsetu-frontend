6820a6325aaf046530a74c54e5d7ca4f7bbe513b
Arman M Jatti <123095283+jattiarman@users.noreply.github.com>
2025-09-29 23:23:23 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders the main header with the Code Translator link', () => {
+test('renders learn react link', () => {
   render(<App />);
-  
-  // Look for the "Code Translator" link in the document
-  const linkElement = screen.getByText(/Code Translator/i);
-  
-  // Assert that this link is actually present
+  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
